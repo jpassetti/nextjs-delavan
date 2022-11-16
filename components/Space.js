dbc1a4fe58626a9504a36acb9a53200a0c0e5fb2
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2022-11-16 16:07:07 -0500

    spaces

--- components/Space.js
+++ components/Space.js
@@ -1,4 +1,5 @@
 import Heading from './Heading';
+import Paragraph from './Paragraph';
 
 import styles from './space.module.scss';
 
@@ -6,11 +7,12 @@ const Space = ({space}) => {
     const {title, size, description, featuredImage} = space;
     return <div 
     className={styles.space}
-    style={{backgroundImage: `url(${featuredImage.node.sourceUrl})`}}
     >
+        <div className={styles.space__image} style={{backgroundImage: `url(${featuredImage.node.sourceUrl})`}}></div>
         <div className={styles.space__content}>
-            <Heading level="2" color="white" marginBottom="2">{title}</Heading>
-            <Heading level="3" color="white">{size}</Heading>
+            <Heading level="2" color="white" marginBottom="1">{title}</Heading>
+            <Heading level="3" color="white" marginBottom="2" fontWeight="400">{size}</Heading>
+            <Paragraph color="white">{description}</Paragraph>
         </div>
     </div>
 }
