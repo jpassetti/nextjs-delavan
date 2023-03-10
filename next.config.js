a24b22ceea3c02bfcfecb6194a4ac0e81989e7e3
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2023-03-10 12:16:18 -0500

    updated next.config.js to include new domain

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ module.exports = {
         protocol: 'https',
         hostname: 'delavan-studios-dev.com'
       },
+      {
+        protocol: 'http',
+        hostname: 'delavan.stayfresh.design'
+      },
     ],
   }
 }
